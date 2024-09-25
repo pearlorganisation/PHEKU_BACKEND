3bb6dcfaba46639adf487f85b93f82f20f59f3d1
Ranjan <ranjan@pearlorganisation.com>
2024-09-25 17:26:59 +0530

    fixed create course function

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -2,23 +2,46 @@ import Course from "../models/course.js";
 import ApiError from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-
-// to create a course 
-export const createCourse =asyncHandler(async(req,res)=>{
-
-    const { title, university, slug, duration, courseLevel, examType, fees, location , specialization} = req.body;
-  // to check if the course already exists
-  const courseExists = await Course.findOne({ title });
-  const sameUniverity = await Course.findOne({ university });
-if(courseExists && sameUniverity){
-    res.status(400);
-    throw new Error("Course already exists in the University");
-}const course = new Course({ title, university, slug, duration,    courseLevel, examType, fees, location , specialization})
-
-    const createdCourse = await course.save();
-    res.status(201).json(createdCourse)
-
-});
+import University from "../models/university.js";
+ 
+// Create course handle
+export const createCourse = asyncHandler(async (req, res) => {
+    const { title, universitySlug, slug, duration, courseLevel, examType, fees, location, specialization } = req.body;
+  
+    // Find the university by its slug
+    const university = await University.findOne({ slug: universitySlug });
+  
+    if (!university) {
+      res.status(404);
+      throw new Error("University not found");
+    }
+  
+    // Check if the course already exists in the same university
+    const courseExists = await Course.findOne({ title, university: university._id });
+  
+    if (courseExists) {
+      res.status(400);
+      throw new Error("Course already exists in the University");
+    }
+  
+    // Create a new course with the university reference
+    const course = new Course({
+      title,
+      university: university._id, // Referencing the university by ObjectId
+      slug,
+      duration,
+      courseLevel,
+      examType,
+      fees,
+      location,
+      specialization,
+    });
+  if(!course){
+    return res.status(400).json("Unable to create Course",400)
+  } const createdCourse = await course.save();
+    res.status(201).json(createdCourse);
+  });
+  
 
 // to get all the course
 export const getAllCourse =  asyncHandler(async(req,res)=>{
