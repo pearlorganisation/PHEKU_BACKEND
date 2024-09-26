8bbde3a949571b9ade583d66869a6bac5845fc12
Ranjan <ranjan@pearlorganisation.com>
2024-09-26 11:04:35 +0530

    created updateById for course

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -2,25 +2,24 @@ import Course from "../models/course.js";
 
 import { asyncHandler } from "../utils/asyncHandler.js";
 import University from "../models/university.js";
- 
+import ApiError from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js"; 
 // Create course handle
-export const createCourse = asyncHandler(async (req, res) => {
+export const createCourse = asyncHandler(async (req, res,next) => {
     const { title, universitySlug, slug, duration, courseLevel, examType, fees, location, specialization } = req.body;
   
     // Find the university by its slug
     const university = await University.findOne({ slug: universitySlug });
   
     if (!university) {
-      res.status(404);
-      throw new Error("University not found");
+     return next(new ApiError("Unable to find the university",400))
     }
   
     // Check if the course already exists in the same university
     const courseExists = await Course.findOne({ title, university: university._id });
   
     if (courseExists) {
-      res.status(400);
-      throw new Error("Course already exists in the University");
+      return next( new ApiError("Course already exists in the current university",400))
     }
   
     // Create a new course with the university reference
@@ -36,31 +35,29 @@ export const createCourse = asyncHandler(async (req, res) => {
       specialization,
     });
   if(!course){
-    return res.status(400).json("Unable to create Course",400)
+    return next(new ApiError("Failed to create the course try again",400))
   } const createdCourse = await course.save();
-    res.status(201).json(createdCourse);
+    return res.status(201).json(new ApiResponse("Course created successfully",createdCourse,200));
   });
   
 
 // to get all the course
-export const getAllCourse =  asyncHandler(async(req,res)=>{
+export const getAllCourse =  asyncHandler(async(req,res,next)=>{
 
     const course = await Course.find();
     if(course.length ===0){
-        res.status(400);
-        throw new Error("No Course Found")
+        return next(new ApiError("No course available",404))
     }
-    res.status(200).json(course)
+    return res.status(200).json(new ApiResponse("Fetched all the courses", course, 200))
 })
 
 // to get the course by id
 
-export const getCourseById = asyncHandler(async(req,res)=>{
+export const getCourseById = asyncHandler(async(req,res,next)=>{
     const course = await Course.findById(req.params?.id)
     if(!course){
-        res.status(400);
-        throw new Error("Course not found")
-    }return res.status(200).json(course);
+        return next(new ApiError("Unable to get the course",404))
+    }return res.status(200).json(new ApiResponse("Fetched the course",course,200));
 })
 
 
@@ -68,8 +65,38 @@ export const deleteById= asyncHandler(async(req,res,next)=>{
     const remCourse = await Course.findByIdAndDelete(req.params?.id);
 
     if(!remCourse){
-    throw new Error("No Course found")
+    return next(new ApiError("Failed to delete the course",400))
     } return res
     .status(200)
-    .json("Course deleted successfully", null, 200);;
+    .json(new ApiResponse("Successfully removed the course",null, 200))
+})
+
+// funtionn to updateCourse
+
+export const updateById = asyncHandler(async(req,res,next)=>{
+   const courseId = req.params?.id;
+console.log(courseId)
+  const { title, duration, courseLevel, examType, fees, location, specialization } = req.body;
+console.log( title )
+const updatedCourse = await Course.findByIdAndUpdate(
+  courseId,
+  {
+    $set: {
+      title,
+      "duration.minDuration": duration?.minDuration,
+      "duration.maxDuration": duration?.maxDuration,
+      "fees.minAmount": fees?.minAmount,
+      "fees.maxAmount": fees?.maxAmount,
+      courseLevel,
+      examType,
+      location,
+      specialization,
+    },
+  },
+  { new: true } // Return the updated document
+);
+
+if (!updatedCourse) {
+  return next(new ApiError("Failed to update the course", 400));
+}return res.json(new ApiResponse("Successfully updated the course ",updatedCourse,200));
 })
--- backend/src/routes/courseRoutes.js
+++ backend/src/routes/courseRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 
-import { createCourse , deleteById, getAllCourse, getCourseById } from "../controllers/courseController.js";
+import { createCourse , deleteById, getAllCourse, getCourseById, updateById } from "../controllers/courseController.js";
 
 const router = express.Router()
 router.route("/").post(createCourse).get(getAllCourse);
-router.route("/:id").get(getCourseById).delete(deleteById);
+router.route("/:id").get(getCourseById).patch(updateById).delete(deleteById);
 export default router
