781272dfe4b379b16392d8871454cfb6beab8a2c
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-10-18 16:53:28 +0530

    cloudinary config

--- backend/src/controllers/universityController.js
+++ backend/src/controllers/universityController.js
@@ -2,6 +2,7 @@ import University from "../models/university.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js"
 import ApiError from "../utils/ApiError.js"
+
 export const createUniversity = asyncHandler(async (req, res, next) => {
   const {
     name,
