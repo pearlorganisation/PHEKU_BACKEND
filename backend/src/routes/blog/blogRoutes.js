9ba7f1d5a068f6a9b368ba066fa94757d889a2e5
Ranjan <ranjan@pearlorganisation.com>
2024-10-31 12:05:40 +0530

    minor changes

--- backend/src/routes/blog/blogRoutes.js
+++ backend/src/routes/blog/blogRoutes.js
@@ -21,23 +21,24 @@ router
   .route("/")
   .post(createBlog) // Create a blog post
   .get(getAllBlogs); // Get all blog posts
+  router
+    .route("/categories")
+    .post(createBlogCategory) // Create a blog category
+    .get(getBlogCategories); // Get all blog categories
+
+    // Define the routes for BLOG CATEGORY
+
+    router
+      .route("/categories/:id")
+      .get(getBlogCategoryById) // Get a blog category by ID
+      .delete(deleteBlogCategoryById) // Delete a blog category by ID
+      .put(updateBlogCategoryById); // Update a blog category by ID
+
 
 router
   .route("/:id")
   .get(getBlogById) // Get a blog post by ID
   .delete(deleteBlogbyId) // Delete a blog post by ID
   .put(updateBlogById); // Update a blog post by ID
-
-// Define the routes for BLOG CATEGORY
-router
-  .route("/categories")
-  .post(createBlogCategory) // Create a blog category
-  .get(getBlogCategories); // Get all blog categories
-
-router
-  .route("/categories/:id")
-  .get(getBlogCategoryById) // Get a blog category by ID
-  .delete(deleteBlogCategoryById) // Delete a blog category by ID
-  .put(updateBlogCategoryById); // Update a blog category by ID
-
+ 
 export default router;
