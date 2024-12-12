6e3d1aeff774a172c0e5f9748b8279993129d071
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-12-12 18:02:29 +0530

    update middleware

--- backend/src/models/university.js
+++ backend/src/models/university.js
@@ -10,27 +10,27 @@ const facultySchema = new mongoose.Schema({
 
 const universitySchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
-    slug: { type: String, required: true, unique: true },
-    overview: { type: String }, // Edditor, frontend will send raw html
-    highlights: { type: String }, // Edditor
-    facilities: { type: String }, // Edditor
-    coverPhoto: { secure_url: { type: String }, public_id: { type: String } },
-    logo: { secure_url: { type: String }, public_id: { type: String } },
+    // name: { type: String, required: true, unique: true },
+    // slug: { type: String, required: true, unique: true },
+    // overview: { type: String }, // Edditor, frontend will send raw html
+    // highlights: { type: String }, // Edditor
+    // facilities: { type: String }, // Edditor
+    // coverPhoto: { secure_url: { type: String }, public_id: { type: String } },
+    // logo: { secure_url: { type: String }, public_id: { type: String } },
     country: { type: mongoose.Schema.Types.ObjectId, ref: "Country" }, // Filtering
-    state: { type: String, required: true },
-    district: { type: String, required: true },
-    city: { type: String, required: true },
-    address: { type: String, required: true },
-    estdYear: { type: String },
-    email: { type: String },
-    location: { type: String, required: true }, // Embeded link of google map location
-    website: { type: String, required: true },
-    phone: { type: String },
-    ranking: { global: Number, national: Number },
-    totalCourse: { type: Number, default: 0 }, // No need to parse these from string to num
-    totalRating: { type: Number, default: 0 },
-    faculties: [facultySchema], // Array of faculty members
+    // state: { type: String, required: true },
+    // district: { type: String, required: true },
+    // city: { type: String, required: true },
+    // address: { type: String, required: true },
+    // estdYear: { type: String },
+    // email: { type: String },
+    // location: { type: String, required: true }, // Embeded link of google map location
+    // website: { type: String, required: true },
+    // phone: { type: String },
+    // ranking: { global: Number, national: Number },
+    // totalCourse: { type: Number, default: 0 }, // No need to parse these from string to num
+    // totalRating: { type: Number, default: 0 },
+    // faculties: [facultySchema], // Array of faculty members
   },
   { timestamps: true }
 );
@@ -52,6 +52,37 @@ universitySchema.post("findOneAndDelete", async function (doc) {
     }
   } catch (error) {
     console.error("Error during post deletion:", error);
+  }
+});
+
+universitySchema.post("findOneAndUpdate", async function (doc) {
+  try {
+    // Ensure the document exists
+    if (!doc) {
+      console.log("No document found to process on update.");
+      return;
+    }
+
+    // Get the filter used for the query
+    const filter = this.getFilter();
+
+    // Fetch the document before the update
+    const oldDoc = await this.model.findOne(filter);
+    console.log(oldDoc);
+    if (!oldDoc) {
+      console.log("Old document not found for updating totalUniversities.");
+      return;
+    }
+
+    // Update the count for the old country
+    await updateTotalUniversitiesForCountry(oldDoc.country);
+
+    // Update the count for the new country if it has changed
+    if (String(oldDoc.country) !== String(doc.country)) {
+      await updateTotalUniversitiesForCountry(doc.country);
+    }
+  } catch (error) {
+    console.error("Error during post update:", error.message);
   }
 });
 
