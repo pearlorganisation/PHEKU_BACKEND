7b47da6960173b95dd2c5a233cdc43b6ab756fc2
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-12-17 18:48:53 +0530

    uncomment

--- backend/src/models/university.js
+++ backend/src/models/university.js
@@ -10,27 +10,27 @@ const facultySchema = new mongoose.Schema({
 
 const universitySchema = new mongoose.Schema(
   {
-    // name: { type: String, required: true, unique: true },
-    // slug: { type: String, required: true, unique: true },
-    // overview: { type: String }, // Edditor, frontend will send raw html
-    // highlights: { type: String }, // Edditor
-    // facilities: { type: String }, // Edditor
-    // coverPhoto: { secure_url: { type: String }, public_id: { type: String } },
-    // logo: { secure_url: { type: String }, public_id: { type: String } },
+    name: { type: String, required: true, unique: true },
+    slug: { type: String, required: true, unique: true },
+    overview: { type: String }, // Edditor, frontend will send raw html
+    highlights: { type: String }, // Edditor
+    facilities: { type: String }, // Edditor
+    coverPhoto: { secure_url: { type: String }, public_id: { type: String } },
+    logo: { secure_url: { type: String }, public_id: { type: String } },
     country: { type: mongoose.Schema.Types.ObjectId, ref: "Country" }, // Filtering
-    // state: { type: String, required: true },
-    // district: { type: String, required: true },
-    // city: { type: String, required: true },
-    // address: { type: String, required: true },
-    // estdYear: { type: String },
-    // email: { type: String },
-    // location: { type: String, required: true }, // Embeded link of google map location
-    // website: { type: String, required: true },
-    // phone: { type: String },
-    // ranking: { global: Number, national: Number },
-    // totalCourse: { type: Number, default: 0 }, // No need to parse these from string to num
-    // totalRating: { type: Number, default: 0 },
-    // faculties: [facultySchema], // Array of faculty members
+    state: { type: String, required: true },
+    district: { type: String, required: true },
+    city: { type: String, required: true },
+    address: { type: String, required: true },
+    estdYear: { type: String },
+    email: { type: String },
+    location: { type: String, required: true }, // Embeded link of google map location
+    website: { type: String, required: true },
+    phone: { type: String },
+    ranking: { global: Number, national: Number },
+    totalCourse: { type: Number, default: 0 }, // No need to parse these from string to num
+    totalRating: { type: Number, default: 0 },
+    faculties: [facultySchema], // Array of faculty members
   },
   { timestamps: true }
 );
